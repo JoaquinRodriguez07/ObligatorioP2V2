add98bf71031933e11d727a759358d4932b27513
. <joaquinrzg@gmail.com>
2025-06-01 12:53:26 -0300

    ClasePeliculas
    Se completo la clase Peliculas

--- grupo12-P2-tads/build.gradle.kts
+++ grupo12-P2-tads/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    //lombok
     compileOnly("org.projectlombok:lombok:1.18.38")
     annotationProcessor("org.projectlombok:lombok:1.18.38")
     testCompileOnly("org.projectlombok:lombok:1.18.38")
