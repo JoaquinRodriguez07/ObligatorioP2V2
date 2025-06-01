f0eb70e210b678f36f5a10be081dfccc5be16a0b
. <joaquinrzg@gmail.com>
2025-06-01 12:09:15 -0300

    Modulo clases

--- grupo12-P2-tads/build.gradle.kts
+++ grupo12-P2-tads/build.gradle.kts
@@ -13,10 +13,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    //lombokcompileOnly("org.projectlombok:lombok:1.18.38")
-    	annotationProcessor("org.projectlombok:lombok:1.18.38")
-    	testCompileOnly("org.projectlombok:lombok:1.18.38")
-    	testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+    testCompileOnly("org.projectlombok:lombok:1.18.38")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
 }
 
 tasks.test {
