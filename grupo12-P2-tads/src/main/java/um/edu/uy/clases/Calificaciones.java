0d12f01bedbd3721f3d09d0694b26de0e2a8528f
mliceran <nasza24yt@gmail.com>
2025-06-01 12:50:05 -0300

    4 clases creadas

---
+++ grupo12-P2-tads/src/main/java/um/edu/uy/clases/Calificaciones.java
@@ -0,0 +1,10 @@
+package um.edu.uy.clases;
+
+import java.util.Date;
+
+public class Calificaciones {
+    private Usuario usuario;
+    private String idPelicula;
+    private double puntaje;
+    private Date fecha;
+}
---
+++ grupo12-P2-tads/src/main/java/um/edu/uy/clases/Personas.java
@@ -0,0 +1,6 @@
+package um.edu.uy.clases;
+
+public class Personas {
+    private String nombre;
+    //private List<String> rol;
+}
